32321e3baf25c37421570f62a2e43d800d78f84a
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2023-03-03 20:11:28 -0800

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./twitter.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Tweetle</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
