ea865345d08a6bc0efccd96e77272218853c236f
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2023-03-04 02:18:17 -0800

    Implemented axios request of the user timeline in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,22 @@ import logo from "./twitter.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Input from "./components/Input";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [username, setUsername] = useState("");
+  function loadUserTweets() {
+    Axios.get("http://localhost:3001/api/tweets/" + username).then(
+      (response) => {
+        console.log(response);
+      }
+    );
+  }
   return (
     <div className="App">
       <Navbar />
-      <Input />
+      <Input setUsername={setUsername} loadUserTweets={loadUserTweets} />
     </div>
   );
 }
--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./Input.css";
 
-export default function Input() {
+export default function Input(props) {
+  function handleClick() {
+    props.setUsername(document.getElementById("input_id").value);
+    props.loadUserTweets();
+  }
   return (
     <div className="input-div">
       <div className="form">
         <label className="input-label">enter a twitter @</label>
         <input
+          id="input_id"
           className="input"
           type="text"
           name="user"
           placeholder="boofbeanz"
         />
-        <button className="input-button">play</button>
+        <button className="input-button" onClick={handleClick}>
+          play
+        </button>
       </div>
     </div>
   );
