216bedc0e05b279ae08376815e974ac2b0e0ef39
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2023-03-04 01:34:09 -0800

    Added API request for getting user's timeline. Working on Navbar

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -1,7 +1,21 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Twitter = require("twitter");
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+const client = new Twitter({
+  consumer_key: process.env.API,
+  consumer_secret: process.env.API_SECRET,
+  access_token_key: process.env.ACCESS,
+  access_token_secret: process.env.ACCESS_SECRET,
+});
+router.get("/tweets/:user", async (req, res, next) => {
+  const screen_name = req.params.user;
+  try {
+    const tweets = await client.get("statuses/user_timeline", { screen_name });
+    res.send(tweets);
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
 });
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import logo from "./twitter.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Tweetle</h3>
-      </header>
+      <Navbar />
     </div>
   );
 }
