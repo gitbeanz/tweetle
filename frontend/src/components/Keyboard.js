ddf02510b73d60f8fac0b6a7307361fa961fdc5b
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2023-03-20 03:54:52 -0700

    Reformatted keyboard component and made it accessible. Added enter and delete key

--- frontend/src/components/Keyboard.js
+++ frontend/src/components/Keyboard.js
@@ -1,46 +1,50 @@
 import React from "react";
 import "./Keyboard.css";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBackspace } from "@fortawesome/free-solid-svg-icons";
 
 export default function Keyboard(props) {
-  const [letters, setLetters] = useState([
-    { key: "a" },
-    { key: "b" },
-    { key: "c" },
-    { key: "d" },
-    { key: "e" },
-    { key: "f" },
-    { key: "g" },
-    { key: "h" },
-    { key: "i" },
-    { key: "j" },
-    { key: "k" },
-    { key: "l" },
-    { key: "m" },
-    { key: "n" },
-    { key: "o" },
-    { key: "p" },
-    { key: "q" },
-    { key: "r" },
-    { key: "s" },
-    { key: "t" },
-    { key: "u" },
-    { key: "v" },
-    { key: "w" },
-    { key: "x" },
-    { key: "y" },
-    { key: "z" },
-  ]);
+  const first_row_letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
+  const second_row_letters = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
+  const third_row_letters = ["z", "x", "c", "v", "b", "n", "m"];
   console.log(props.usedKeys);
   return (
     <div className="keyboard">
-      {letters.map((letter) => {
-        return (
-          <div className={props.usedKeys[letter.key]} key={letter.key}>
-            {letter.key}
-          </div>
-        );
-      })}
+      <div className="first-row">
+        {first_row_letters.map((letter) => {
+          return (
+            <button id="key" className={props.usedKeys[letter]} key={letter}>
+              {letter}
+            </button>
+          );
+        })}
+      </div>
+      <div className="second-row">
+        {second_row_letters.map((letter) => {
+          return (
+            <button id="key" className={props.usedKeys[letter]} key={letter}>
+              {letter}
+            </button>
+          );
+        })}
+      </div>
+      <div className="third-row">
+        <button className="big-button" key="Enter">
+          ENTER
+        </button>
+
+        {third_row_letters.map((letter) => {
+          return (
+            <button id="key" className={props.usedKeys[letter]} key={letter}>
+              {letter}
+            </button>
+          );
+        })}
+        <button className="big-button" key="Del">
+          <FontAwesomeIcon className="font-icon" icon={faBackspace} />
+        </button>
+      </div>
     </div>
   );
 }
